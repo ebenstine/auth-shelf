d38749a3af40b1d1152981828b0ce1a2f4292dfa
Michael Werneke <mike.werneke@gmail.com>
2021-09-03 13:38:36 -0500

    debuggin

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -23,12 +23,13 @@ router.get("/",rejectUnauthenticated, (req, res) => {
 /**
  * Add an item for the logged in user to the shelf
  */
-router.post("/",rejectUnauthenticated, (req, res) => {
+router.post('/', rejectUnauthenticated, (req, res) => {
   
 
   // endpoint functionality
-  const queryText = `INSERT INTO "item"("description", "image_url", "user_id")
-  VALUES ($1,$2,$3)`;
+  const queryText = `INSERT INTO "item" ("description", "image_url", "user_id")
+  VALUES ($1, $2, $3)`;
+  console.log('This is Req', req.user.id);
     pool.query(queryText, [req.body.description, req.body.image_url, req.user.id])
     .then((result)=>{
       
--- src/redux/sagas/post.saga.js
+++ src/redux/sagas/post.saga.js
@@ -1,13 +1,16 @@
 import axios from "axios"
 import {takeLatest, put} from 'redux-saga/effects'
 
+
+
 function* postShelf() {
+  
     try{
         const res = yield axios.post('/api/shelf', action.payload);
-        console.log(action.payload);
+        
         yield put ({
-            type:'SET_SHELF',
-            payload: res.data
+            type:'FETCH_SHELF',
+            
         });
     }catch (err) {
         console.log( 'Post shelf', err);
